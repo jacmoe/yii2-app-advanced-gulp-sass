40011f15b69c5e9c122b1c75ac5db0dc897921d8
Jacob Moen <jacmoe.dk@gmail.com>
2016-03-12 17:38:25 +0100

    Navbar logout menu item fix

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -38,14 +38,7 @@ use common\widgets\Alert;
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
     } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link']
-            )
-            . Html::endForm()
-            . '</li>';
+        $menuItems[] = '<li>' . Html::a('Logout', ['/user/logout'], ['data-method' => 'post']).'</li>';
     }
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
